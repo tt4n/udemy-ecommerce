17137f38bdb1b7680bb10f1601f419c53adb320c
Tian Tan <tian@tribepad.com>
2019-08-05 22:59:41 +0100

    add directory

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css'
 
-import HomePage from "./components/homepage.component";
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div>
       <HomePage />
-    </div>
   );
 }
 
