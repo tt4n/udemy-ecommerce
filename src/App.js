4589abde8d5b6b613059a65bce714a895cf966e1
Tian Tan <tian@tribepad.com>
2019-08-13 22:49:24 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css'
-import { BrowserRouter as Router, Route, Link  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 const Hat = () => {
   return <h1>Hats</h1>;
@@ -12,10 +13,15 @@ const Hat = () => {
 
 function App() {
   return (
-      <Router>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-      </Router>
+    <Router>
+        <div>
+            <Header></Header>
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route exact path='/shop' component={ShopPage} />
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
